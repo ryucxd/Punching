b94cdf678bf838df39b21cb1454091ea1d4a0ad6
ryucxd <ryucxd@gmail.com>
2021-06-15 15:57:23 +0100

    Add project files.

---
+++ Punching/frmMain.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace Punching
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lbl = new System.Windows.Forms.Label();
+            this.lblHours = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnExtensions = new System.Windows.Forms.Button();
+            this.btnStock = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnItems = new System.Windows.Forms.Button();
+            this.btnComplete = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtDoorLookup = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.txtBatchID = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tempLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 189);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(713, 440);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl.Location = new System.Drawing.Point(428, 68);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(207, 20);
+            this.lbl.TabIndex = 1;
+            this.lbl.Text = "Work Available at Shift Start";
+            // 
+            // lblHours
+            // 
+            this.lblHours.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHours.Location = new System.Drawing.Point(428, 88);
+            this.lblHours.Name = "lblHours";
+            this.lblHours.Size = new System.Drawing.Size(207, 18);
+            this.lblHours.TabIndex = 2;
+            this.lblHours.Text = "label2";
+            this.lblHours.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Punching.Properties.Resources.logo;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 56);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(225, 93);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnExtensions
+            // 
+            this.btnExtensions.Location = new System.Drawing.Point(12, 12);
+            this.btnExtensions.Name = "btnExtensions";
+            this.btnExtensions.Size = new System.Drawing.Size(75, 34);
+            this.btnExtensions.TabIndex = 4;
+            this.btnExtensions.Text = "Extensions";
+            this.btnExtensions.UseVisualStyleBackColor = true;
+            // 
+            // btnStock
+            // 
+            this.btnStock.Location = new System.Drawing.Point(93, 12);
+            this.btnStock.Name = "btnStock";
+            this.btnStock.Size = new System.Drawing.Size(75, 34);
+            this.btnStock.TabIndex = 5;
+            this.btnStock.Text = "Manage Stock";
+            this.btnStock.UseVisualStyleBackColor = true;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(174, 12);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 34);
+            this.btnLoad.TabIndex = 6;
+            this.btnLoad.Text = "View Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            // 
+            // btnItems
+            // 
+            this.btnItems.Location = new System.Drawing.Point(255, 12);
+            this.btnItems.Name = "btnItems";
+            this.btnItems.Size = new System.Drawing.Size(75, 34);
+            this.btnItems.TabIndex = 7;
+            this.btnItems.Text = "Bending Items";
+            this.btnItems.UseVisualStyleBackColor = true;
+            // 
+            // btnComplete
+            // 
+            this.btnComplete.Location = new System.Drawing.Point(336, 12);
+            this.btnComplete.Name = "btnComplete";
+            this.btnComplete.Size = new System.Drawing.Size(75, 34);
+            this.btnComplete.TabIndex = 8;
+            this.btnComplete.Text = "Mark Complete";
+            this.btnComplete.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button6.Location = new System.Drawing.Point(650, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 34);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Log Off";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button7);
+            this.groupBox1.Controls.Add(this.txtDoorLookup);
+            this.groupBox1.Location = new System.Drawing.Point(294, 68);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(128, 93);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Door Lookup";
+            // 
+            // txtDoorLookup
+            // 
+            this.txtDoorLookup.Location = new System.Drawing.Point(9, 33);
+            this.txtDoorLookup.Name = "txtDoorLookup";
+            this.txtDoorLookup.Size = new System.Drawing.Size(111, 20);
+            this.txtDoorLookup.TabIndex = 0;
+            this.txtDoorLookup.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtDoorLookup_KeyDown);
+            this.txtDoorLookup.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDoorLookup_KeyPress);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(27, 59);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 1;
+            this.button7.Text = "Search";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // txtBatchID
+            // 
+            this.txtBatchID.Location = new System.Drawing.Point(115, 164);
+            this.txtBatchID.Name = "txtBatchID";
+            this.txtBatchID.Size = new System.Drawing.Size(100, 20);
+            this.txtBatchID.TabIndex = 11;
+            this.txtBatchID.TextChanged += new System.EventHandler(this.txtBatchID_TextChanged);
+            this.txtBatchID.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.txtBatchID.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 18);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Batch ID Search";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // tempLabel
+            // 
+            this.tempLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tempLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tempLabel.Location = new System.Drawing.Point(221, 164);
+            this.tempLabel.Name = "tempLabel";
+            this.tempLabel.Size = new System.Drawing.Size(504, 23);
+            this.tempLabel.TabIndex = 13;
+            this.tempLabel.Text = "Work Available at Shift Start";
+            this.tempLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.tempLabel.Visible = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(737, 641);
+            this.Controls.Add(this.tempLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBatchID);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.btnComplete);
+            this.Controls.Add(this.btnItems);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnStock);
+            this.Controls.Add(this.btnExtensions);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblHours);
+            this.Controls.Add(this.lbl);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Batch List";
+            this.Shown += new System.EventHandler(this.frmMain_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.Label lblHours;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnExtensions;
+        private System.Windows.Forms.Button btnStock;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnItems;
+        private System.Windows.Forms.Button btnComplete;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox txtDoorLookup;
+        private System.Windows.Forms.TextBox txtBatchID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label tempLabel;
+    }
+}
+
---
+++ Punching/frmMain.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Punching
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+            string sql = "select id as [Batch ID],qname as [Description],machine as [Machine],datecomplete as [Date Complete],qcomplete as [QComplete] from dbo.batch_header  order by qid desc";
+            using (SqlConnection conn = new SqlConnection(CONNECT.ConnectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    DataTable dt = new DataTable();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                    formatting();
+                }
+                sql = "exec usp_get_department_work_availability 'Punching'";
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    lblHours.Text = cmd.ExecuteScalar().ToString();
+                }
+                conn.Close();
+            }
+
+
+            //start a timer
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Interval = 60000; //every 60 seconds
+            timer.Elapsed += timer_Elapsed;
+            timer.Start();
+        }
+
+        private void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            //this is the code that will fire every 60seconds
+            //dbo.bending_stock_items
+            bendingAlert();
+        }
+
+        private void bendingAlert()
+        {
+            string sql = "select [description],requested_amount from dbo.bending_stock_items where request_yn = -1";
+            using (SqlConnection conn = new SqlConnection(CONNECT.ConnectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    DataTable dt = new DataTable();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(dt);
+                    //read from the dt 
+                    if (dt.Rows.Count > 1)
+                    {
+                        tempLabel.Invoke((Action)delegate//only way to change controls while there is multiple threads running ???
+                        {
+                            tempLabel.Text = "**MULTIPLE LOW STOCK ITEMS**";
+                            tempLabel.Visible = true;
+                            tempLabel.BackColor = Color.PaleVioletRed;
+                        });
+                    }
+                    else if (dt.Rows.Count == 1)
+                    {
+                        tempLabel.Invoke((Action)delegate
+                        {
+                            tempLabel.Text = "Bending Dpt Requested: " + dt.Rows[0][0].ToString() + " x " + dt.Rows[0][1].ToString();
+                            tempLabel.Visible = true;
+                            tempLabel.BackColor = Color.PaleVioletRed;
+                        });
+                    }
+                    else
+                        tempLabel.Invoke((Action)delegate
+                        {
+                            tempLabel.Visible = false;
+                        });
+                }
+                conn.Close();
+            }
+        }
+
+        private void formatting()
+        {
+            try
+            {
+                dataGridView1.Columns.Remove("Complete");
+            }
+            catch
+            {
+            }
+            //also add the button here too
+            DataGridViewButtonColumn completeButtonColumn = new DataGridViewButtonColumn();
+            completeButtonColumn.Name = "Complete";
+            completeButtonColumn.Text = "Complete";
+            completeButtonColumn.UseColumnTextForButtonValue = true;
+            int columnIndex = (dataGridView1.Columns.Count);
+            if (dataGridView1.Columns["Complete"] == null)
+            {
+                dataGridView1.Columns.Insert(columnIndex, completeButtonColumn);
+            }
+            dataGridView1.Columns[4].Visible = false;
+            dataGridView1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                int index = dataGridView1.Columns["QComplete"].Index;
+                if (Convert.ToInt32(row.Cells[index].Value) == -1)
+                    row.DefaultCellStyle.BackColor = Color.DarkSeaGreen;
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int index = dataGridView1.Columns["Complete"].Index;
+            if (e.ColumnIndex == index)
+            {
+                MessageBox.Show("Complete clicked");
+            }
+        }
+
+        private void frmMain_Shown(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                int index = dataGridView1.Columns["QComplete"].Index;
+                if (Convert.ToInt32(row.Cells[index].Value) == -1)
+                    row.DefaultCellStyle.BackColor = Color.DarkSeaGreen;
+            }
+        }
+
+        private void txtDoorLookup_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Enter)
+            {
+                doorLookup(); //this can fire from two places so its easier to just stick it in its own void
+            }
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            doorLookup();
+        }
+
+        private void doorLookup()
+        {
+            string sql = "SELECT dbo.batch_header.qid FROM dbo.batch_programs INNER JOIN dbo.batch_header ON dbo.batch_programs.header_id = dbo.batch_header.id " +
+                "where door_id = " + txtDoorLookup.Text + " GROUP BY dbo.batch_header.qid, dbo.batch_programs.door_id ";
+
+            using (SqlConnection conn = new SqlConnection(CONNECT.ConnectionString))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    var getdata = cmd.ExecuteScalar();
+                    if (getdata != null)
+                        MessageBox.Show("Door found on batch " + getdata.ToString());
+                    else
+                        MessageBox.Show("Door not found on any batch");
+
+                    txtDoorLookup.Text = "";
+
+                }
+                conn.Close();
+            }
+        }
+
+        private void txtDoorLookup_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                applyFilter(); //this can fire from two places so its easier to just stick it in its own void
+            }
+        }
+
+        private void applyFilter()
+        {
+            try
+            {
+                dataGridView1.Columns.Remove("Complete");
+            }
+            catch
+            {
+            }
+            if (txtBatchID.TextLength > 0)
+            {
+                dataGridView1.DataSource = null;
+                //filter batch id
+                string sql = "select id as [Batch ID],qname as [Description],machine as [Machine],datecomplete as [Date Complete],qcomplete as [QComplete] from dbo.batch_header where id like '%" + txtBatchID.Text + "%'  order by qid desc";
+                using (SqlConnection conn = new SqlConnection(CONNECT.ConnectionString))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        DataTable dt = new DataTable();
+                        SqlDataAdapter da = new SqlDataAdapter(cmd);
+                        da.Fill(dt);
+                        dataGridView1.DataSource = dt;
+                        formatting();
+                    }
+                }
+            }
+            else
+            {
+                string sql = "select id as [Batch ID],qname as [Description],machine as [Machine],datecomplete as [Date Complete],qcomplete as [QComplete] from dbo.batch_header  order by qid desc";
+                using (SqlConnection conn = new SqlConnection(CONNECT.ConnectionString))
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = new SqlCommand(sql, conn))
+                    {
+                        DataTable dt = new DataTable();
+                        SqlDataAdapter da = new SqlDataAdapter(cmd);
+                        da.Fill(dt);
+                        dataGridView1.DataSource = dt;
+                        formatting();
+                    }
+                }
+            }
+
+        }
+
+        private void txtBatchID_TextChanged(object sender, EventArgs e)
+        {
+            applyFilter();
+        }
+
+
+
+        private void markAsComplete(int batch_id)
+        {
+            //this is the big one
+
+
+            string sql = "";
+
+            using (SqlConnection conn = new SqlConnection(CONNECT.ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+
+                }
+            }
+        }
+    }
+}
